e52232ab47cf5df01321bcdcf1828617e9424412
RamSaw <mikhail@pravilov.com>
2019-02-01 18:37:44 +0300

    Added comments to Pipeline class.

--- hw01/src/main/kotlin/ru/hse/spb/pipeline/Pipeline.kt
+++ hw01/src/main/kotlin/ru/hse/spb/pipeline/Pipeline.kt
@@ -1,7 +1,17 @@
 package ru.hse.spb.pipeline
 
+/**
+ * Class that simulates pipeline. Pipeline is quite similar to ordinary file.
+ */
 class Pipeline(private val content: String) {
+    /**
+     * Returns file name for pipe.
+     * By default it is empty.
+     */
     fun getFileName(): String = ""
 
+    /**
+     * Returns content of pipe.
+     */
     fun getContent(): String = content
 }
