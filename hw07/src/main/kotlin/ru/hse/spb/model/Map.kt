58d6c520aebc9e3c23aa15697f368a323c702f9f
VadimFarutin <vfarut@gmail.com>
2019-03-12 01:07:15 +0300

    Loading Map from file

--- hw07/src/main/kotlin/ru/hse/spb/model/Map.kt
+++ hw07/src/main/kotlin/ru/hse/spb/model/Map.kt
@@ -3,16 +3,16 @@ package ru.hse.spb.model
 import java.io.File
 import kotlin.random.Random
 
-class Map private constructor(private val field: Array<Array<CellState>>) {
+class Map private constructor(val field: Array<Array<CellState>>) {
     companion object Factory {
-        private const val FIELD_WIDTH = 10
-        private const val FIELD_HEIGHT = 10
+        private const val DEFAULT_FIELD_WIDTH = 10
+        private const val DEFAULT_FIELD_HEIGHT = 10
         private const val WALL_PERCENTAGE = 25
 
         private val random = Random(0)
 
         fun generate(): Map {
-            val field = Array(FIELD_HEIGHT) { Array(FIELD_WIDTH) { CellState.FREE } }
+            val field = Array(DEFAULT_FIELD_HEIGHT) { Array(DEFAULT_FIELD_WIDTH) { CellState.FREE } }
 
             for (row in field) {
                 for (i in row.indices) {
@@ -27,14 +27,17 @@ class Map private constructor(private val field: Array<Array<CellState>>) {
         }
 
         fun load(path: String): Map {
-            val (height, width) = File(path).inputStream().bufferedReader().readLine()!!.split(' ').map(String::toInt)
+            val reader = File(path).inputStream().bufferedReader()
+            val (height, width) = reader.readLine().split(' ').map(String::toInt)
             val field = Array(height) { Array(width) { CellState.FREE } }
 
             for (row in field) {
+                val currentLine = reader.readLine()
+
                 for (i in row.indices) {
-                    row[i] = when (random.nextInt(0, 100)) {
-                        in 0..WALL_PERCENTAGE -> CellState.WALL
-                        else -> CellState.FREE
+                    row[i] = when (currentLine[i]) {
+                        '.' -> CellState.FREE
+                        else -> CellState.WALL
                     }
                 }
             }
