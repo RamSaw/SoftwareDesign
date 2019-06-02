d737078bf3bf9b72638f801a8b33c0b1920cadad
RamSaw <mikhail@pravilov.com>
2019-06-02 23:48:48 +0300

    Fixed program arguments parsing. Now exceptions are caught and handled.

--- hw07/src/main/kotlin/ru/hse/spb/RougelikeApplication.kt
+++ hw07/src/main/kotlin/ru/hse/spb/RougelikeApplication.kt
@@ -4,35 +4,46 @@ import ru.hse.spb.controller.Controller
 import ru.hse.spb.model.Map
 import ru.hse.spb.model.WorldModel
 import ru.hse.spb.view.ConsoleView
-import java.lang.IllegalArgumentException
+import java.io.FileNotFoundException
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     val view = ConsoleView
     val map = when (args.size) {
         0 -> Map.generate()
-        1 -> Map.load(args[0])
-        2 -> {
-            val width = args[0].toInt()
-            val height = args[1].toInt()
-            if (width < 3 || height < 3) {
-                println("Width and height must be at least 3")
-                printHelp()
-                exitProcess(1)
+        1 -> {
+            try {
+                Map.load(args[0])
+            } catch (e: FileNotFoundException) {
+                exitWithError("File not found. Error message: " + e.message)
             }
-            Map.generate(width, height)
         }
-        else -> {
-            println("More then 2 arguments is not supported!")
-            printHelp()
-            exitProcess(1)
+        2 -> {
+            try {
+                val width = args[0].toInt()
+                val height = args[1].toInt()
+
+                if (width < 3 || height < 3) {
+                    exitWithError("Width and height must be at least 3")
+                }
+                Map.generate(width, height)
+            } catch (e: NumberFormatException) {
+                exitWithError("You've passed not integers. Error message: " + e.message)
+            }
         }
+        else -> exitWithError("More then 2 arguments is not supported!")
     }
-    val model = WorldModel(map, view)
+    val model = WorldModel(map as Map, view)
     val controller = Controller(model, view)
 
     controller.run()
     view.stop()
+}
+
+fun exitWithError(errorMessage: String) {
+    System.out.println(errorMessage)
+    printHelp()
+    exitProcess(1)
 }
 
 fun printHelp() {
