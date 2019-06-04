c41ad85770e5cba002e0cbe83f4363a312cbdaf1
Vladimir Egorov <vladimirrim98@gmail.com>
2019-06-04 20:05:08 +0300

    Delete unnecessary files.

--- hw11/send_receive_message.py
+++ hw11/send_receive_message.py
@@ -2,7 +2,6 @@ import sys
 import boto3
 from time import sleep
 
-
 def get_queue_by_name(name):
     queue_initialized = False
     queue = None
@@ -15,12 +14,10 @@ def get_queue_by_name(name):
             sleep(1)
     return queue
 
-
 my_queue_name = sys.argv[1]
 queue_to_send_name = sys.argv[2]
-sleep(15)  # wait for queues to become initialized
-sqs = boto3.resource('sqs', endpoint_url='http://localstack:4576', aws_access_key_id="access",
-                     aws_secret_access_key="secret")
+sleep(15) # wait for queues to become initialized
+sqs = boto3.resource('sqs', endpoint_url='http://localstack:4576/', aws_access_key_id="access", aws_secret_access_key="secret")
 my_queue = get_queue_by_name(my_queue_name)
 queue_to_send = get_queue_by_name(queue_to_send_name)
 while True:
@@ -28,4 +25,5 @@ while True:
         new_num = str(int(message.body) + 1)
         print("Queue " + my_queue_name + " received " + message.body, flush=True)
         message.delete()
-        queue_to_send.send_message(MessageBody=new_num)
+        queue_to_send.send_message(MessageBody=(new_num))
+
